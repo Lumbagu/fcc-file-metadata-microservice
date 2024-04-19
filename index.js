9800fb9e1fdf056e434080f112051dcc4b627d1c
Lumbagu <masiver.ae@yahoo.com>
2024-04-19 23:23:54 +0200

    my solution

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+const multer = require("multer");
+const upload = multer();
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    console.log(req.file);
+    if (!req.file) {
+        return res.json({ error: "No file to analyse" });
+    }
+
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
